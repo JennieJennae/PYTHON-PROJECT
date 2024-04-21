1f45e91e30dceced21c9906e153ace36a1d8deee
JennieJennae <122867649+JennieJennae@users.noreply.github.com>
2024-04-21 15:42:01 +0100

    Update appointment.py

--- schema/appointment.py
+++ schema/appointment.py
@@ -13,14 +13,14 @@ class Appointment(BaseModel):
 appointment_data = [
     {
         "id": 11,
-        "patient": {"id": 0, "name": "Ahmed", "age": 50, "sex": "M", "weight": 100, "height": 22, "phone": "+2348036782925"},
-        "doctor": {"id": 1, "name": "Falomo", "specialization": "Anesthesiology", "phone": "+2347164859515", "is_available": True},
+        "patient": {"id": 0, "name": "Adamma", "age": 50, "sex": "M", "weight": 100, "height": 22, "phone": "+2348036782925"},
+        "doctor": {"id": 1, "name": "Falana", "specialization": "Anesthesiology", "phone": "+2347164859515", "is_available": True},
         "appointment_time": "2024-04-22T12:00:00"  
     },
     {
         "id": 12,
-        "patient": {"id": 1, "name": "Beatrice", "age": 11, "sex": "F", "weight": 90, "height": 19, "phone": "+234330000000"},
-        "doctor": {"id": 2, "name": "Aderinokun", "specialization": "Dermatology", "phone": "+2347061703817", "is_available": True},
+        "patient": {"id": 1, "name": "Rayna", "age": 11, "sex": "F", "weight": 90, "height": 19, "phone": "+234330000000"},
+        "doctor": {"id": 2, "name": "Tokunbo", "specialization": "Dermatology", "phone": "+2347061703817", "is_available": True},
         "appointment_time": "2024-04-20T14:00:00" 
     }
 ]
