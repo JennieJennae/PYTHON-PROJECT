893651d7bc539726273559fd30412b2c8ec67ff8
JennieJennae <122867649+JennieJennae@users.noreply.github.com>
2024-04-21 23:18:55 +0100

    Update appointment.py

--- schema/appointment.py
+++ schema/appointment.py
@@ -1,6 +1,6 @@
 from pydantic import BaseModel
 from schema.patients import Patient
-from schema.doctors import Doctors
+from schema.doctors import Doctors, Status
 from datetime import datetime
 
 class Appointment(BaseModel):
@@ -9,20 +9,3 @@ class Appointment(BaseModel):
     doctor: Doctors
     appointment_time: datetime
     
-
-appointment_data = [
-    {
-        "id": 11,
-        "patient": {"id": 0, "name": "Adamma", "age": 50, "sex": "M", "weight": 100, "height": 22, "phone": "+2348036782925"},
-        "doctor": {"id": 1, "name": "Falana", "specialization": "Anesthesiology", "phone": "+2347164859515", "is_available": True},
-        "appointment_time": "2024-04-22T12:00:00"  
-    },
-    {
-        "id": 12,
-        "patient": {"id": 1, "name": "Rayna", "age": 11, "sex": "F", "weight": 90, "height": 19, "phone": "+234330000000"},
-        "doctor": {"id": 2, "name": "Tokunbo", "specialization": "Dermatology", "phone": "+2347061703817", "is_available": True},
-        "appointment_time": "2024-04-20T14:00:00" 
-    }
-]
-
-appointments = [Appointment(**data) for data in appointment_data]
